b583708d1e615a06add17af8298da87492e7c31a
MrHenger <henry.r0701@gmail.com>
2022-01-29 00:24:12 -0400

    feat: se crearon resources para category, image, post y user
    Esto para poder transformar o estructurar de una manera mas limpia
    la data que se responde

---
+++ app/Http/Resources/ImageResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ImageResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->route,
+            'fullPatch' => asset('/images/miniatures/'.$this->route),
+        ];
+    }
+}
